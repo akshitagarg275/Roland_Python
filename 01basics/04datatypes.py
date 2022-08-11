1e8b24a62682086d855f44479091359492e76a74
akshitagarg275 <akshitagarg275@gmail.com>
2022-08-11 08:14:18 +0530

    day 4

--- 01basics/04datatypes.py
+++ 01basics/04datatypes.py
@@ -37,3 +37,59 @@
 # temp = None
 # print("Temp is : ",temp)
 # print(type(temp))
+
+# TODO: strings 
+# it is one line string
+# s = '"we are learning python"'
+# print(s)
+# print(type(s))
+
+# s = "Let's begin the journey"
+# print(s)
+# print(type(s))
+
+# s = ''' Hi
+# We are learning
+# about strings
+# today in the class
+# '''
+
+# print(s)
+# print(type(s))
+
+
+# TODO: derived datatypes
+
+# TODO: list -> []
+# list allows to store homogeneous/ heterogeneous values in a single variable name
+# fruits = ['apple','banana' , 'mango', 'grapes']
+# print(fruits)
+# print(type(fruits))
+
+# TODO : tuple -> ()
+# fruits = ('apple','banana' , 'mango', 'grapes')
+# print(fruits)
+# print(type(fruits))
+
+# TODO: set {}
+# fruits = {'apple','banana' , 'mango', 'grapes'}
+# print(fruits)
+# print(type(fruits))
+
+# TODO: dictionary -> {key : value}
+# stu = {
+#     "fname" : "John",
+#     "lname" : "Doe",
+#     "age" : 15,
+#     "course" : "Python"
+# }
+
+# print(stu)
+# print(type(stu))
+
+# TODO: range -> it generates the numbers within a certain range
+#  range(start , end + 1 , step)
+
+nums = list(range(1, 11))
+print(nums)
+print(type(nums))
