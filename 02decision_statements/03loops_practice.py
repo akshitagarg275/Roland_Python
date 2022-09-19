a5c65bcfd97c240273bd5f5c6257b995ae60ee43
akshitagarg275 <akshitagarg275@gmail.com>
2022-09-19 08:23:56 +0530

    day 14

--- 02decision_statements/03loops_practice.py
+++ 02decision_statements/03loops_practice.py
@@ -1,12 +1,41 @@
 #WAP to find number of digits entered by a user
 
+# num = int(input("Enter a number: "))
+# count = 0
+# while num>0:
+#     count = count +1
+#     num = num//10
+
+# else:
+#     print("No of digits: ",count)
 
 
-num = int(input("Enter a number: "))
-count = 0
-while num>0:
-    count = count +1
-    num = num//10
+# WAP to add all the digits entered by the user
+# 564 -> 5 + 6 + 4 = 15
+# 123 -> 1 + 2 + 3 = 6
 
-else:
-    print("No of digits: ",count)
+
+# num = int(input("Enter a num :"))
+# sum = 0
+
+# while num > 0 :
+#     rem = num % 10
+#     sum = sum + rem
+#     num = num // 10
+
+# print("Sum of digits is: ",sum)
+ 
+
+# WAP : to reverse a given number
+# 564 -> 465
+
+num = int(input("Enter a num :"))
+rev= 0
+
+while num > 0 :
+    rem = num % 10
+    rev = (rev * 10 ) + rem
+    print("Inside: ",rev)
+    num = num //10
+
+print("Reverese is :",rev)
---
+++ 02decision_statements/04jump_statements.py
@@ -0,0 +1,24 @@
+'''
+jump statements are used to modify the
+normal execution of the python statements
+
+break -> to stop
+continue -> to skip
+pass ->
+'''
+
+# for i in range(1,6):
+#     if i == 3:
+#         break
+#     print(i)
+
+# for i in range(1,6):
+#     if i == 3:
+#         continue
+#     print(i)
+
+for i in range(1,6):
+    if i == 3:
+        pass
+    print(i)
+
